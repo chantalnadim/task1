6f52c3ed6eb5e2c55791e8bab1676149b21e27a2
chantalnadim <chantalnadim@gmail.com>
2024-08-07 15:13:45 +0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+     <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}" />
+  </head>
+  <body>
+    <div class="resume">
+      <div class="resume_left">
+        <div class="resume_profile">
+          <img src="{{asset('frontend/imgs/chanty.png')}}" alt="profile_pic" />
+        </div>
+        <div class="resume_content">
+          <div class="resume_item resume_info">
+            <div class="title">
+              <p class="bold">CHANTAL NADIM</p>
+              <p class="regular">software developer</p>
+            </div>
+            <ul>
+              <li>
+                <div class="icon">
+                  <i class="fas fa-map-signs"></i>
+                </div>
+                <div class="data">
+                  cournish AL-tijara <br />
+                  Damascus
+                </div>
+              </li>
+              <li>
+                <div class="icon">
+                  <i class="fas fa-mobile-alt" aria-hidden="true"></i>
+                </div>
+                <div class="data">0930778286</div>
+              </li>
+              <li>
+                <div class="icon">
+                  <i class="fas fa-envelope"></i>
+                </div>
+                <div class="data">chantal@gmail.com</div>
+              </li>
+
+            </ul>
+          </div>
+          <div class="resume_item resume_skills">
+            <div class="title">
+              <p class="bold">skill's</p>
+            </div>
+            <ul>
+              <li>
+                <div class="skill_name">HTML</div>
+                <div class="skill_progress">
+                  <span style="width: 80%"></span>
+                </div>
+                <div class="skill_per">80%</div>
+              </li>
+              <li>
+                <div class="skill_name">CSS</div>
+                <div class="skill_progress">
+                  <span style="width: 70%"></span>
+                </div>
+                <div class="skill_per">70%</div>
+              </li>
+              <li>
+                <div class="skill_name">Laravel</div>
+                <div class="skill_progress">
+                  <span style="width: 90%"></span>
+                </div>
+                <div class="skill_per">90%</div>
+              </li>
+              <li>
+                <div class="skill_name">JS</div>
+                <div class="skill_progress">
+                  <span style="width: 60%"></span>
+                </div>
+                <div class="skill_per">60%</div>
+              </li>
+              <li>
+                <div class="skill_name">SQL</div>
+                <div class="skill_progress">
+                  <span style="width: 88%"></span>
+                </div>
+                <div class="skill_per">88%</div>
+              </li>
+            </ul>
+          </div>
+          <div class="resume_item resume_social">
+            <div class="title">
+              <p class="bold">Social</p>
+            </div>
+            <ul>
+              <li>
+                <div class="icon">
+                  <i class="fab fa-facebook-square"></i>
+                </div>
+                <div class="data">
+                  <p class="semi-bold">Facebook</p>
+                  <p>Chantalnadim@facebook</p>
+                </div>
+              </li>
+              <li>
+                <div class="icon">
+                  <i class="fab fa-twitter-square"></i>
+                </div>
+                <div class="data">
+                  <p class="semi-bold">Linkedin</p>
+                  <p>chantalnadim@linkedin</p>
+                </div>
+              </li>
+
+
+<div class="resume_item resume_social">
+            <div class="title">
+              <p class="bold">Language</p>
+            </div>
+
+
+              <li>
+
+                <div class="data">
+                  <p class="semi-bold">Arabic</p>
+                  <p>Native Language</p>
+                </div>
+              </li>
+              <li>
+                <div class="data">
+                  <p class="semi-bold">English</p>
+                  <p>good</p>
+                </div>
+              </li>
+
+
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="resume_right">
+        <div class="resume_item resume_about">
+          <div class="title">
+            <p class="bold">PROFILE</p>
+          </div>
+          <p>
+            I am a passionate and curious individual with an insatiable love for
+            learning. As a girl who finds great joy in acquiring knowledge and
+            expanding my horizons, I constantly seek out new opportunities to
+            challenge myself intellectually
+          </p>
+        </div>
+        <div class="resume_item resume_work">
+          <div class="title">
+            <p class="bold">Work Experience</p>
+          </div>
+          <ul>
+            <li>
+              <div class="date">2022 – 2024</div>
+              <div class="info">
+                <p class="semi-bold">Freelancer website developer</p>
+                <p>
+                  I worked on make awesome websites for my clients using Laravel
+                  as backend word press as frontend
+                </p>
+              </div>
+
+ </div>
+
+
+        <div class="resume_item resume_projects">
+  <div class="title">
+    <p class="bold">PROJECTS</p>
+  </div>
+  <ul>
+    <li>
+      <div class="date">2023</div>
+      <div class="info">
+        <p class="semi-bold">My pharma (inventory management system)</p>
+        <p>
+          I worked on making easy access and reactive system using Laravel as a backend implementing CRUD operations and design patterns.
+        </p>
+      </div>
+    </li>
+    <li>
+      <div class="date">2024</div>
+      <div class="info">
+        <p class="semi-bold">Fitness (fitness app)</p>
+        <p>
+          Fitness is a sports application that replaces the need to go to the gym. I worked on the backend side using Laravel, applying notification using Pusher and CRUD operations using MVC architecture.
+        </p>
+      </div>
+    </li>
+  </ul>
+</div>
+
+<div class="resume_item resume_courses">
+  <div class="title">
+    <p class="bold">COURSES</p>
+  </div>
+  <ul>
+    <li>
+      <div class="date">2021</div>
+      <div class="info">
+        <p class="semi-bold">Website design</p>
+        <p>
+          At Markaz al-Hadara. HTML/CSS Proficiency, UX Design Principles, Responsive Design, Website Optimization (Performance, SEO), CMS Proficiency (WordPress), Collaboration, Problem-Solving, Attention to Detail, Analytics.
+        </p>
+      </div>
+    </li>
+    <li>
+      <div class="date">2020</div>
+      <div class="info">
+        <p class="semi-bold">WordPress</p>
+        <p>
+          At the Training and Employment Center in the College of Information Engineering I learned how to navigate the WordPress interface, customizing website themes, utilizing plugins, and managing website content. Additionally, I have developed expertise in search engine optimization (SEO) techniques for improving website visibility and attracting organic traffic.
+        </p>
+      </div>
+    </li>
+    <li>
+      <div class="date">2019</div>
+      <div class="info">
+        <p class="semi-bold">Photoshop</p>
+        <p>
+          Munazamat Gouba.
+        </p>
+      </div>
+    </li>
+  </ul>
+</div>
+        <div class="resume_item resume_education">
+          <div class="title">
+            <p class="bold">Education</p>
+          </div>
+          <ul>
+            <li>
+              <div class="date"> 2021 -2026</div>
+              <div class="info">
+                <p class="semi-bold"> Information
+ Technology Engineering</p>
+                <p>
+                   Damascus University
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="date">2020 - 2021</div>
+              <div class="info">
+                <p class="semi-bold">Scientific Baccalaureate</p>
+                <p>
+                  Damascus-Syria
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+
+        </div>
+      </div>
+    </div>
+</body>
+</html>
